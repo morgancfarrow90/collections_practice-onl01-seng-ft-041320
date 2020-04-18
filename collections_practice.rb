1606319f1cdf62327bd0a10b3646668f90090b1b
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 21:14:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,13 +58,4 @@ end
 new_array
 end
 
-    def kesha_maker(array)
-  kesha = []
-  array.each do |word|
-    word_array = word.split ""
-    word_array[2] = "$"
-    kesha << word_array.join
-  end
-  kesha
-end
 
