7ccf31340fc5cb29db49453520d4fe344a0ce691
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 20:48:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,8 +37,7 @@ end
 sort_array_char_count(strings)
 
 def swap_elements(array)
-   array.sort do |a,b|
-     array[a], array [b] = array[2], array[1]
+     array[1], array [2] = array[2], array[1]
  end
 
 swap_elements(myarray)
