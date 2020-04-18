355ab69b2703f4cda619e19baef1cf33d71dbe8c
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 20:27:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+myarray = [8,1,4,5,9,3]
 
+def sort_array_asc(array)
+  array.sort 
+  end
+  
+sort_array_asc(myarray)
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+    0
+  elsif a < b
+    1 
+  elsif a > b
+    -1
+  end
+end
+end
+
+sort_array_desc(myarray)
+
+strings = ["cat", "duck", "elephant"]
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0 
+    elsif a.length < b.length
+    -1 
+  elsif a.length > b.length
+    1
+  end
+end
+end
+      
+sort_array_char_count(strings)
+
+def swap_elements(array)
+  
